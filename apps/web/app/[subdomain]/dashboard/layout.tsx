5c37635cb501ed29ddfd316f81f55fb2774e7012
ahmergamer4 <ahmedalseidy4@gmail.com>
2025-04-10 21:39:16 +0200

    Add: breadcrumb ui component and fix dashboard layout

--- apps/web/app/[subdomain]/dashboard/layout.tsx
+++ apps/web/app/[subdomain]/dashboard/layout.tsx
@@ -14,25 +14,7 @@ import { getSession } from "@/lib/session";
 import { Book, ChartBar, Settings } from "lucide-react";
 import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
-import { SidebarItem } from "./sidebar-item";
-
-const items = [
-  {
-    title: "Courses",
-    url: "/dashboard/courses",
-    // icon: Book,
-  },
-  {
-    title: "Analytics",
-    url: "/dashboard/analytics",
-    // icon: ChartBar,
-  },
-  {
-    title: "Settings",
-    url: "/dashboard/settings",
-    // icon: Settings,
-  },
-];
+import { SidebarItems } from "./sidebar-item";
 
 export default async function DashboardLayout({ children }: PropsWithChildren) {
   const session = await getSession();
@@ -41,10 +23,10 @@ export default async function DashboardLayout({ children }: PropsWithChildren) {
 
   return (
     <SidebarProvider>
-      <div className="min-h-screen flex w-full">
+      <div className="flex min-h-screen w-full">
         <SidebarTrigger />
         <Sidebar>
-          <SidebarHeader className="font-bold text-xl mt-6 ml-3">
+          <SidebarHeader className="ml-3 mt-3 text-xl font-bold">
             {session.user.name[0]?.toUpperCase() +
               session.user.name.substring(1)}{" "}
             Platform
@@ -53,14 +35,12 @@ export default async function DashboardLayout({ children }: PropsWithChildren) {
             <SidebarGroup>
               <SidebarGroupLabel>Items</SidebarGroupLabel>
               <SidebarGroupContent className="flex flex-col gap-2">
-                {items.map((item) => (
-                  <SidebarItem key={item.title} item={item} />
-                ))}
+                <SidebarItems />
               </SidebarGroupContent>
             </SidebarGroup>
           </SidebarContent>
         </Sidebar>
-        <div className="py-8 w-full">{children}</div>
+        <div className="w-full py-2">{children}</div>
       </div>
     </SidebarProvider>
   );
--- apps/web/app/[subdomain]/dashboard/sidebar-item.tsx
+++ apps/web/app/[subdomain]/dashboard/sidebar-item.tsx
@@ -1,28 +1,50 @@
 "use client";
 
 import { SidebarMenuItem, SidebarMenuButton } from "@/components/ui/sidebar";
+import { Book, ChartBar, Settings } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import path from "path";
 
-export const SidebarItem = ({ item }: { item: any }) => {
+const items = [
+  {
+    title: "Courses",
+    url: "/dashboard/courses",
+    icon: Book,
+  },
+  {
+    title: "Analytics",
+    url: "/dashboard/analytics",
+    icon: ChartBar,
+  },
+  {
+    title: "Settings",
+    url: "/dashboard/settings",
+    icon: Settings,
+  },
+];
+
+export const SidebarItems = () => {
   const path = usePathname();
   return (
-    <SidebarMenuItem key={item.title} className="list-none ">
-      <SidebarMenuButton asChild>
-        <Link
-          className={
-            item.url === path
-              ? `bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground`
-              : ``
-          }
-          replace={true}
-          href={item.url}
-        >
-          {/* <item.icon /> */}
-          <span className="font-semibold text-md">{item.title}</span>
-        </Link>
-      </SidebarMenuButton>
-    </SidebarMenuItem>
+    <>
+      {items.map((item) => (
+        <SidebarMenuItem key={item.title} className="list-none">
+          <SidebarMenuButton asChild>
+            <Link
+              className={
+                item.url === path
+                  ? `bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground`
+                  : ``
+              }
+              replace={true}
+              href={item.url}
+            >
+              <item.icon />
+              <span className="text-md font-semibold">{item.title}</span>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+    </>
   );
 };
