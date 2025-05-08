e922211238293facdd14f4ec51131966fb99d938
ahmergamer4 <ahmedalseidy4@gmail.com>
2025-05-09 00:38:15 +0300

    feat(videos): update video management to support lesson-based structure; add video retrieval and deletion functionality

--- apps/api/src/videos/videos.controller.ts
+++ apps/api/src/videos/videos.controller.ts
@@ -2,12 +2,21 @@ import { Roles } from '@/auth/decorators/roles.decorator';
 import { S3Service } from '@/s3/s3.service';
 import { User } from '@/users/decorators/user.decorator';
 import { UploadVideoDto } from '@lms-saas/shared-lib';
-import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  InternalServerErrorException,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { VideosService } from './videos.service';
 
 @ApiBearerAuth()
-@Controller('courses/:courseId/sections/:sectionId/videos')
+@Controller('courses/:courseId/sections/:sectionId/lessons/:lessonId/videos')
 export class VideosController {
   constructor(
     private s3Service: S3Service,
@@ -18,13 +27,27 @@ export class VideosController {
   @Roles('teacher')
   async uploadVideo(
     @User() teacher: any,
-    @Param('sectionId', ParseIntPipe) sectionId: number,
+    @Param('lessonId', ParseIntPipe) lessonId: number,
     @Body() dto: UploadVideoDto,
   ) {
-    const key = `videos/${teacher.id}/${sectionId}/${Date.now()}-${dto.title}.mp4`;
-    const url = await this.s3Service.uploadVideo(key, 'video/mp4');
+    const key = `videos/${teacher.id}/${lessonId}/${Date.now()}-${dto.title}.mp4`;
+    const s3Res = await this.s3Service.uploadVideo(key, 'video/mp4');
 
-    await this.videosService.create(sectionId, key, dto);
-    return { ...url };
+    const res = await this.videosService.create(lessonId, key, dto);
+    return { ...s3Res, videoDetails: res };
+  }
+
+  @Delete('/:videoId')
+  @Roles('teacher')
+  delete(@Param('videoId', ParseIntPipe) videoId: number) {
+    return this.videosService.delete(videoId);
+  }
+
+  @Get('/:videoId')
+  @Roles('teacher', 'student')
+  async getVideoUrl(@Param('videoId', ParseIntPipe) videoId: number) {
+    const video = await this.videosService.getVideo(videoId);
+    const url = await this.s3Service.getSignedUrl(video[0].s3Key);
+    return { videoId: video[0].id, url };
   }
 }
--- apps/api/src/videos/videos.service.ts
+++ apps/api/src/videos/videos.service.ts
@@ -1,13 +1,37 @@
 import { db, UploadVideoDto, videos } from '@lms-saas/shared-lib';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class VideosService {
-  async create(sectionId: number, s3Key: string, dto: UploadVideoDto) {
+  async create(lessonId: number, s3Key: string, dto: UploadVideoDto) {
     try {
-      await db.insert(videos).values({ ...dto, sectionId, s3Key });
+      return db
+        .insert(videos)
+        .values({ ...dto, lessonId, s3Key })
+        .returning({
+          id: videos.id,
+          title: videos.title,
+          s3Key: videos.s3Key,
+        });
     } catch (error) {
       throw new InternalServerErrorException(`Cannot create video. ${error}`);
+    }
+  }
+
+  async delete(videoId: number) {
+    try {
+      await db.delete(videos).where(eq(videos.id, videoId));
+    } catch (error) {
+      throw new InternalServerErrorException(`Cannot remove video. ${error}`);
+    }
+  }
+
+  async getVideo(videoId: number) {
+    try {
+      return db.select().from(videos).where(eq(videos.id, videoId));
+    } catch (error) {
+      throw new InternalServerErrorException(`Cannot get video. ${error}`);
     }
   }
 }
