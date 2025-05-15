62add70a67c4cbfc4313e65e3fe7124be80b188c
ahmergamer4 <ahmedalseidy4@gmail.com>
2025-05-15 15:35:55 +0300

    refactor(auth): made signup page client side

--- apps/web/app/[subdomain]/(auth)/(student)/signup/page.tsx
+++ apps/web/app/[subdomain]/(auth)/(student)/signup/page.tsx
@@ -1,13 +1,12 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { SignupForm } from "./signup-form";
+import { useParams } from "next/navigation";
 
-export default async function SignupPage({
-  params,
-}: {
-  params: { subdomain: string };
-}) {
-  const { subdomain } = await params;
+export default async function SignupPage() {
+  const { subdomain } = useParams();
   return (
     <div className="flex w-full items-center justify-center">
       <div className="bg-primary text-primary-foreground hidden min-h-screen w-1/2 flex-col justify-between p-12 md:flex">
@@ -22,7 +21,7 @@ export default async function SignupPage({
       <div className="flex w-10/12 justify-center md:w-1/2">
         <div className="w-80">
           <h1 className="mb-4 text-center text-2xl font-bold">Sign In Page</h1>
-          <SignupForm subdomain={subdomain} />
+          <SignupForm subdomain={subdomain as string} />
           <div className="mt-2 flex justify-between text-sm">
             <p>Already have an account?</p>
             <Link className="underline" href={"/login"}>
