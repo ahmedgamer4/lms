c786fb8582a6c7f15539cd087bfabd2592d681bd
ahmergamer4 <ahmedalseidy4@gmail.com>
2025-05-16 17:44:45 +0300

    feat(editor): integrate Lexical editor with custom toolbar and block formatting options

---
+++ apps/web/components/blocks/editor-00/plugins.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@/components/editor/editor-ui/content-editable";
+import { ToolbarPlugin } from "@/components/editor/plugins/toolbar/toolbar-plugin";
+import { FormatQuote } from "@/components/editor/plugins/toolbar/block-format/format-quote";
+import { FormatCheckList } from "@/components/editor/plugins/toolbar/block-format/format-check-list";
+import { BlockFormatDropDown } from "@/components/editor/plugins/toolbar/block-format-toolbar-plugin";
+import { FormatBulletedList } from "@/components/editor/plugins/toolbar/block-format/format-bulleted-list";
+import { FormatHeading } from "@/components/editor/plugins/toolbar/block-format/format-heading";
+import { FormatNumberedList } from "@/components/editor/plugins/toolbar/block-format/format-numbered-list";
+import { FormatParagraph } from "@/components/editor/plugins/toolbar/block-format/format-paragraph";
+import { HistoryToolbarPlugin } from "@/components/editor/plugins/toolbar/history-toolbar-plugin";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { FontFormatToolbarPlugin } from "@/components/editor/plugins/toolbar/font-format-toolbar-plugin";
+
+export function Plugins() {
+  const [floatingAnchorElem, setFloatingAnchorElem] =
+    useState<HTMLDivElement | null>(null);
+
+  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
+    if (_floatingAnchorElem !== null) {
+      setFloatingAnchorElem(_floatingAnchorElem);
+    }
+  };
+
+  return (
+    <div className="relative">
+      {/* toolbar plugins */}
+
+      <ToolbarPlugin>
+        {({ blockType }) => (
+          <div className="vertical-align-middle sticky top-0 z-10 flex gap-2 overflow-auto border-b p-1">
+            <HistoryToolbarPlugin />
+
+            <BlockFormatDropDown>
+              <FormatParagraph />
+              <FormatHeading levels={["h1", "h2", "h3"]} />
+              <FormatNumberedList />
+              <FormatBulletedList />
+              <FormatCheckList />
+              <FormatQuote />
+            </BlockFormatDropDown>
+
+            <FontFormatToolbarPlugin format="bold" />
+            <FontFormatToolbarPlugin format="italic" />
+            <FontFormatToolbarPlugin format="underline" />
+            <FontFormatToolbarPlugin format="strikethrough" />
+          </div>
+        )}
+      </ToolbarPlugin>
+      <div className="relative">
+        <RichTextPlugin
+          contentEditable={
+            <div className="">
+              <div className="" ref={onRef}>
+                <ContentEditable placeholder={"Start typing ..."} />
+              </div>
+            </div>
+          }
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        {/* editor plugins */}
+        <HistoryPlugin />
+      </div>
+      {/* actions plugins */}
+    </div>
+  );
+}
