6c1a0b5d876840c959b5e5f7894d276d47402f95
ahmergamer4 <ahmedalseidy4@gmail.com>
2025-05-23 18:28:15 +0300

    refactor(types): remove unused Role and AsyncWrapperResponse types

--- apps/web/lib/type.ts
+++
@@ -1,5 +0,0 @@
-export type Role = "teacher" | "student";
-
-export type AsyncWrapperResponse<T> =
-  | { error: null; data: T }
-  | { error: any; data: null };
--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { AsyncWrapperResponse } from "./type";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
