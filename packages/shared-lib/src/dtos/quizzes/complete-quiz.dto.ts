d3dc22412c1ed02ce60fd7b4fc352ee25390aaef
ahmergamer4 <ahmedalseidy4@gmail.com>
2025-06-25 15:38:34 +0300

    feat(dtos): add SubmittedAnswer class and update CompleteQuizDto to include answers array for quiz submissions

--- packages/shared-lib/src/dtos/quizzes/complete-quiz.dto.ts
+++ packages/shared-lib/src/dtos/quizzes/complete-quiz.dto.ts
@@ -1,9 +1,34 @@
 // @ts-nocheck
-import { IsNotEmpty, IsNumber, IsPositive } from "class-validator";
+import { Type } from "class-transformer";
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  ValidateNested,
+} from "class-validator";
+
+export class SubmittedAnswer {
+  @IsNotEmpty()
+  @IsPositive()
+  @IsNumber()
+  questionId: number;
+
+  @IsNotEmpty()
+  @IsPositive()
+  @IsNumber()
+  answerId: number;
+}
 
 export class CompleteQuizDto {
   @IsNotEmpty()
   @IsPositive()
   @IsNumber()
   enrollmentId: number;
+
+  @IsNotEmpty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => SubmittedAnswer)
+  answers: SubmittedAnswer[];
 }
