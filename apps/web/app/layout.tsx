0858edef7938a59b000f1ce7b7462c34692714b7
ahmergamer4 <ahmedalseidy4@gmail.com>
2025-07-11 01:49:29 +0300

    feat(layout): enhance RootLayout with internationalization support and custom fonts for improved accessibility and styling

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,20 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import ReactQueryProvider from "@/components/react-query-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { NextIntlClientProvider } from "next-intl";
+import { cookies } from "next/headers";
+import { Noto_Sans_Arabic, Rubik } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const noto = Noto_Sans_Arabic({
+  subsets: ["arabic"],
+  variable: "--font-noto",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  variable: "--font-rubik",
 });
 
 export const metadata: Metadata = {
@@ -19,22 +22,28 @@ export const metadata: Metadata = {
   description: "Next LMS",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const locale = cookieStore.get("NEXT_LOCALE")?.value || "ar";
+  const dir = locale === "ar" ? "rtl" : "ltr";
+
   return (
     <ReactQueryProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <html dir={dir} lang={locale} suppressHydrationWarning>
+        <body className={`${noto.variable} ${rubik.variable}`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <NextIntlClientProvider locale={locale}>
+              {children}
+            </NextIntlClientProvider>
             <Toaster />
           </ThemeProvider>
         </body>
